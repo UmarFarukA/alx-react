d1b6f861ed02c2b5b390b5646bf06630493bbbe3
Umar Faruk Abdulhamid <talk2ufaz@gmail.com>
2023-08-02 22:54:37 +0100

    add task_1

--- 0x03-React_component/task_1/dashboard/src/App/App.js
+++ 0x03-React_component/task_1/dashboard/src/App/App.js
@@ -11,6 +11,11 @@ import { getLatestNotification } from "../utils/utils";
 
 class App extends React.Component {
 
+  constructor() {
+    super(props);
+    this.handleKeyPress = this.handleKeyPress.bind(this);
+  }
+
   listCourses = [
     { id: 1, name: "ES6", credit: 60 },
     { id: 2, name: "Webpack", credit: 20 },
@@ -22,6 +27,22 @@ class App extends React.Component {
     { id: 2, type: "urgent", value: "New resume available" },
     { id: 3, type: "urgent", html: getLatestNotification() },
   ];
+
+  handleKeyPress(evt) {
+    if (evt.ctrlKey && evt.key === 'h') {
+      alert("Logging you out");
+      this.props.logOut();
+    }
+  }
+
+  componentDidMount() {
+    document.addEventListener("keydown", this.handleKeyPress)
+  }
+
+  componentWillUnmount() {
+    document.removeEventListener("keydown", this.handleKeyPress)
+  }
+
   render() {
     return (
       <React.Fragment>
@@ -40,10 +61,14 @@ class App extends React.Component {
 
 App.defaultProps = {
   isLoggedIn: false,
+  logOut: () => {
+    return;
+  }
 };
 
 App.propTypes = {
   isLoggedIn: PropTypes.bool,
+  logOut: PropTypes.func,
 };
 
 export default App;
